b610c4e3f87e9cfa447e91d7c0d6e7ce6a036722
basantbhandari <basantbhandari2074@gmail.com>
2022-06-21 16:41:15 +0545

    day2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // getting the body element
 var body = document.getElementsByTagName("body")[0];
-body.style.border = "3px solid gray";
+body.style.border = "1px solid gray";
 body.style.height = "100vh";
 body.style.width = "100vw";
+
+
